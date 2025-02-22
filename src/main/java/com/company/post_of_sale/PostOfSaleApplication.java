461fc2c34785fbe2174c3cbc8d81e03d6b02facd
Bobur Toshniyozov <boburtoshniyozov4@gmail.com>
2025-02-22 20:31:48 +0500

    "project created"

---
+++ src/main/java/com/company/post_of_sale/PostOfSaleApplication.java
@@ -0,0 +1,13 @@
+package com.company.post_of_sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostOfSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostOfSaleApplication.class, args);
+	}
+
+}
